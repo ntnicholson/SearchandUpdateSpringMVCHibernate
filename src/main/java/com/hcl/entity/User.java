4468a77476418b9071c7e22c99bcb72aa5ab72b4
Nicholas Nicholson <nicholas.nicholson@hcl.com>
2021-01-10 08:53:39 -0500

    created packages

---
+++ src/main/java/com/hcl/entity/User.java
@@ -0,0 +1,25 @@
+package com.hcl.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+	@Id @GeneratedValue
+	private Long id;
+	private String name;
+	private String email;
+}
---
+++ src/main/java/com/hcl/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.hcl.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+
+	
+}
